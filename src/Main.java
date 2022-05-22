ea8261dc05bfecc6bce73f1413980a172f5f79f9
ramiro <dos.ramiro@gmail.com>
2022-05-22 21:31:11 +0200

    Terminado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        suma(4, 7, 9);
+        Car mycar = new Car();
+        mycar.CarDoors();
+        System.out.print(mycar.doors);
+    }
+
+    public static void suma(int a, int b, int c){
+        int result;
+        result = a + b + c;
+        System.out.println(result);
+    }
+}
+
+class Car {
+    public int doors = 3;
+
+    public void CarDoors() {
+        this.doors++;
     }
 }
